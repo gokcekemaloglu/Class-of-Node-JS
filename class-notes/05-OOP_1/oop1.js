21c2e4048c374bbde2602afbe40647ff3a2d34cd
gokcekemaloglu <gokce.kemaloglu@gmail.com>
2024-10-14 12:10:56 +0300

    "Commented out original Car object definition and added new Car object definition with same properties. Uncommented object destructuring code at the end.

--- class-notes/05-OOP_1/oop1.js
+++ class-notes/05-OOP_1/oop1.js
@@ -27,6 +27,70 @@ OOP
 
 // example object
 
+// const Car = {
+//     brand: "Mercedes",
+//     model: "S500",
+//     year: 2020,
+//     isAutoGear: "True",
+//     colors: ["Blue", "Black"],
+//     engine: {
+//         cylinderCount: 8,
+//         hp:100
+//     },
+//     startEngine: function(){
+//         return "engine started"
+//     },
+//     stopEngine: function(){
+//         return "engine stopped"
+//     },
+//     detailFunction: function(){
+//         return this.brand        
+//     },
+//     //! arrow function global scope'dur this i kullanmamız anlamsız kabul etmiyor boş obje döndürüyor
+//     arrowMethod: () => {
+//         return this
+//     }
+// }
+// . dot notaion
+// console.log(Car.brand);
+// console.log(Car.colors);
+// console.log(Car.colors[0]);
+// console.log(Car.engine);
+// console.log(Car.engine.cylinderCount);
+// console.log(Car.startEngine());
+
+// alternative
+
+// console.log(Car["brand"]);
+// console.log(Car["colors"][0]);
+// console.log(Car["stopEngine"]());
+
+//! This keyword
+// içinde bulunduğu objeyi ifade eder
+
+// console.log(Car.detailFunction());
+// console.log(Car.arrowMethod());
+
+//? Array destructuring
+
+const sampleArray = ["val1","val2","val3","val4","val5"]
+// const v1 = sampleArray[0]
+// const v2 = sampleArray[1]
+// const v3 = sampleArray[2]
+// const v4 = sampleArray.slice(2,3)
+
+// console.log(v4);
+
+const [v1, v2, ...vOthers] = sampleArray
+// eşitliğin solunda olacak
+console.log(vOthers);
+
+// spread operator eşitliğin sağı
+const newArray = ["valx",...sampleArray,"valy"]
+console.log(newArray);
+
+//? Obje Destructuring
+
 const Car = {
     brand: "Mercedes",
     model: "S500",
@@ -51,42 +115,18 @@ const Car = {
         return this
     }
 }
-// . dot notaion
-// console.log(Car.brand);
-// console.log(Car.colors);
-// console.log(Car.colors[0]);
-// console.log(Car.engine);
-// console.log(Car.engine.cylinderCount);
-// console.log(Car.startEngine());
 
-// alternative
+// rest
+// const {year, brand, model, ...others} = Car
+// console.log(year, brand, model, others);
+// console.log("******");
+// console.log(Car);
 
-// console.log(Car["brand"]);
-// console.log(Car["colors"][0]);
-// console.log(Car["stopEngine"]());
+const {year, brand: marka, model, ...others} = Car
+console.log(year, marka, model, others);
 
-//! This keyword
-// içinde bulunduğu objeyi ifade eder
 
-console.log(Car.detailFunction());
-console.log(Car.arrowMethod());
 
-//? Array destructuring
 
-const sampleArray = ["val1","val2","val3","val4","val5"]
-// const v1 = sampleArray[0]
-// const v2 = sampleArray[1]
-// const v3 = sampleArray[2]
-// const v4 = sampleArray.slice(2,3)
-
-// console.log(v4);
-
-const [v1, v2, ...vOthers] = sampleArray
-// eşitliğin solunda olacak
-console.log(vOthers);
-
-// spread operator eşitliğin sağı
-const newArray = ["valx",...sampleArray,"valy"]
-console.log(newArray);
 
 
