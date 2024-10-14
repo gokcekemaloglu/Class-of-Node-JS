fdae10437004cfcb8565a271f15f4f65863e8564
gokcekemaloglu <gokce.kemaloglu@gmail.com>
2024-10-14 13:06:32 +0300

    Commented out existing code and added new code for object destructuring, spread operator, JSON conversion, object to array conversion, and constructor functions.

--- class-notes/05-OOP_1/oop1.js
+++ class-notes/05-OOP_1/oop1.js
@@ -81,13 +81,13 @@ const sampleArray = ["val1","val2","val3","val4","val5"]
 
 // console.log(v4);
 
-const [v1, v2, ...vOthers] = sampleArray
+// const [v1, v2, ...vOthers] = sampleArray
 // eşitliğin solunda olacak
-console.log(vOthers);
+// console.log(vOthers);
 
 // spread operator eşitliğin sağı
-const newArray = ["valx",...sampleArray,"valy"]
-console.log(newArray);
+// const newArray = ["valx",...sampleArray,"valy"]
+// console.log(newArray);
 
 //? Obje Destructuring
 
@@ -122,11 +122,69 @@ const Car = {
 // console.log("******");
 // console.log(Car);
 
-const {year, brand: marka, model, ...others} = Car
-console.log(year, marka, model, others);
+// const {year, brand: marka, model, ...others} = Car
+
+// console.log(year, marka, model, others);
+// console.log("***********************");
+// console.log(year, marka, model, Car);
+
+// // spread
+
+// const newCar = {
+//     ...Car,
+//     oilType: "gas"
+// }
+// console.log(newCar);
+
+// obj to JSON
+
+// const jsonCar = JSON.stringify(Car)
+
+// console.log(jsonCar);
+// console.log(typeof jsonCar);
+// console.log(typeof Car);
 
 
+// JSON to obj
 
+// const objCar = JSON.parse(jsonCar)
+
+// console.log(objCar);
+
+// object to Array
+
+// const arrayKeys = Object.keys(Car)
+// console.log(arrayKeys);
+// console.log("**********");
+
+// const arrayValues = Object.values(Car)
+// console.log(arrayValues);
+// console.log("**********");
+
+// const arrayAll = Object.entries(Car)
+// console.log(arrayAll);
+
+// Construction
+
+const constructionFunction = function() {
+    this.property = "value"
+}
+const carConstruction = function(brand, model, year){
+    this.brand = brand,
+    this.model = model,
+    this.year = year,
+    this.startEngine= function(param) {
+        return param
+    }
+}
+const newCar1 = new carConstruction("Volkswagen", "Passat", 2024)
+// console.log(typeof newCar1, newCar1);
+console.log(newCar1.startEngine("Tesla"));
+ 
+console.log(typeof newCar1, newCar1);
+
+const newCar2 = new carConstruction("Audi", "A4", 2023)
+console.log(newCar2.startEngine("AlfaRomeo"));
 
 
 
