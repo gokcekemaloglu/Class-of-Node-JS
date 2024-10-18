e5673730f96b5db8d1e9bdd0459eb2da9abe8ab5
gokcekemaloglu <gokce.kemaloglu@gmail.com>
2024-10-18 10:28:43 +0300

    Added NodeJS setup and usage notes to index.js

--- class-notes/04-JS Recap & NodeJs Setup/index.js
+++ class-notes/04-JS Recap & NodeJs Setup/index.js
@@ -1,1 +1,39 @@
+"use strict"
 console.log("hi there, I am Gokce");
+/* -------------------- NodeJS -------------------- */
+
+// Official: https://nodejs.org
+// Documents: https://nodejs.org/api
+
+/* -------------------- Node Shell -------------------- */
+
+// $ node
+// > console.log('Hello World')
+// > let moduleName = require("module")
+// > moduleName.builtinModules // Show included modules.
+// > .exit // > process.exit()
+
+/* -------------------- NodeJS -------------------- */
+
+let node = 'NodeJS'
+
+console.log(`------*------
+    ${node}
+------*------
+Kodda yaptığımız her bir değişikliği anında görmek istiyorsak nodemon paketini kullanabiliriz.
+`);
+
+/* -------------------- node -------------------- */
+
+// $ node index.js
+// $ node index
+// $ node . # default: index.js
+
+/* -------------------- nodemon -------------------- */
+
+// $ nodemon index.js
+// $ Ctrl + C 
+// $ nodemon # default: index.js
+// $ Ctrl + C 
+
+/* -------------------------------------------- */
