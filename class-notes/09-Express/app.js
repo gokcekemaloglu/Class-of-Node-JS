3615867c086e67256aca7ed629a62d2d00fc10b5
gokcekemaloglu <gokce.kemaloglu@gmail.com>
2024-10-22 00:08:44 +0300

    Updated app.js to include various Express route examples and URL parameter handling

--- class-notes/09-Express/app.js
+++ class-notes/09-Express/app.js
@@ -26,10 +26,60 @@ const HOST = process.env.HOST || "127.0.0.1"
 
 // req and res from request and response
 
-app.get("/",(res, req)=>{
-    res.write("hello")
-    res.write("hello")
-    res.write("hello")
+// app.get("/",(req, res)=>{
+    //? 1
+    // res.write("hello ")
+    // res.write("clarusway")
+    // res.end()
+    //? 2
+    // res.write(JSON.stringify({ // obje'yi yazamaz(bsamaz-render edemez)
+    //     message: "clarusway"
+    // }))    
+    // res.end()
+    //? 3
+    // res.send({ // obje dönüşümünü yapar
+    //     message: "clarusway"
+    // })
+    // res.send("hello clarusway")
+// })
+// app.get("/",(req, res)=>{ res.send("method GET")})
+// app.post("/",(req, res)=>{ res.send("method POST")})
+// app.put("/",(req, res)=>{ res.send("method PUT")})  // PUT = PATCH
+// app.patch("/",(req, res)=>{ res.send("method PATCH")})  // PUT = PATCH
+// app.delete("/",(req, res)=>{ res.send("method DELETE")})
+
+// app.all("/",(req, res)=>{ res.send("method ALL")}) // çok tavsiye edilmiyor
+// app.get("/abc(x)123",(req, res)=>{ res.send("method GET")})
+// app.get("/abc(x?)123",(req, res)=>{ res.send("method GET")}) // x? olabilir de olmayabilir de demek
+// app.get("/abc(x+)123",(req, res)=>{ res.send("method GET")}) // x+ n adet x alabilir demek
+// app.get("/abc(*)123",(req, res)=>{ res.send("method GET")}) //  (*) ne gelirse gelsin demek
+
+// https://regexr.com/
+// regex'de path  ilave etm
+
+// app.get(/abc/,(req, res)=>{ res.send("method GET")}) // /abc/ içersin anlamında ve tırnak olmayacak, ve iki / arasında olacak
+// app.get(/abc$/,(req, res)=>{ res.send("method GET")}) // abc başına ne gelirse gelsin ama en az bir karakter olsun ve sonuna / ekleme
+// app.get(/^\/abc$/,(req, res)=>{ res.send("method GET")}) // abc sonuna ne gelirse gelsin
+
+//? URL parametre
+
+app.get("/user/:userId/:name",(req, res)=>{    
+    res.send({
+        url: {
+            protocol: req.protocol,
+            secure: req.secure,
+            url:req.url,
+            originalURL:req.originalUrl, // hangi routerda isek onu verir ileride bakılacak
+            hostname:req.hostname,
+            subdomain: req.subdomains,
+
+            method:req.method,
+            params:req.params,
+            query:req.query,
+
+        }
+    })
+
 })
 
 
