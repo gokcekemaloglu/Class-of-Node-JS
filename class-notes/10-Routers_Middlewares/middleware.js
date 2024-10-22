25598c3fa8f17d489072981b883a6fc68dc24522
gokcekemaloglu <gokce.kemaloglu@gmail.com>
2024-10-22 11:51:23 +0300

    Updated middleware.js: added new middleware functions, modified existing ones, and changed the final response.

--- class-notes/10-Routers_Middlewares/middleware.js
+++ class-notes/10-Routers_Middlewares/middleware.js
@@ -39,21 +39,63 @@ const HOST=process.env.HOST || '127.0.0.1'
 
 //* Eğer username query ile gönderildiyse next çalışsın, gönderilmediyse hata mesajı verilsin
 
+// app.get("/",(req, res, next)=>{
+//     console.log("middleware 1 çalıştı");
+//     if (req.query.username == "clarusway") {
+//         next()
+//     } else {
+//         res.send({
+//             message: "username not exist"
+//         })
+//     }   
+// })
+
+
+// app.get("/",(req, res)=>{
+//     res.send({
+//         message: "welcome Clarusway"
+//     })
+// })
+
+//?  
+
+// app.get("/",(req, res, next)=>{
+//     if (req.query.username == "clarusway") {
+//         req.message = "welcome"
+//     } else {        
+//         req.message = "username not exist"        
+//     }   
+//     next()
+// })
+
+// app.get("/",(req, res)=>{
+//     res.send({
+//         message: req.message
+//     })
+// })
+
+//?  
+
 app.get("/",(req, res, next)=>{
-    console.log("middleware 1 çalıştı");
-    if (req.query.username == "clarusway") {
-        next()
-    } else {
-        res.send({
-            message: "username not exist"
-        })
-    }   
+    req.message1 = "message from mid 1"      
+    next()
 })
 
+app.get("/",(req, res, next)=>{
+    req.message2 = "message from mid 2"      
+    next()
+})
+
+app.get("/",(req, res, next)=>{
+    req.message3 = "message from mid 3"      
+    next()
+})
 
 app.get("/",(req, res)=>{
     res.send({
-        message: "welcome Clarusway"
+        message1: req.message1,
+        message2: req.message2,
+        message3: req.message3,
     })
 })
 
