f21c8c2b49a78829e08e7de9e3ec56a74eda3937
gokcekemaloglu <gokce.kemaloglu@gmail.com>
2024-10-24 10:32:19 +0300

    Added new route for /user/:id and modified existing code

--- class-notes/11-Error_Management/error.js
+++ class-notes/11-Error_Management/error.js
@@ -18,5 +18,23 @@ const app = express()
 require("dotenv").config()
 const PORT = process.env.PORT || 8000
 
+app.get("/user/:id", (req, res) => {
 
+    const {id} = req.params
+    
+    if (isNaN(id)) {
+        console.log("id is not a number")        
+    } 
+    
+    res.send({
+        error: false,
+        message: "Hi there!",
+        id
+    })
+})
+
+
+
+
+/* ------------------------------------------ */
 app.listen(PORT, ()=>console.log(`Running at: http://127.0.0.1:${PORT}`))
