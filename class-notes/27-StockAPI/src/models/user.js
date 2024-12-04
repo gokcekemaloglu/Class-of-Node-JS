f9ae767e389e698df4dd50e5070e5b0ebaa7097a
gokcekemaloglu <gokce.kemaloglu@gmail.com>
2024-12-04 14:13:58 +0300

    Added password encryption and email/password validation to User model

--- class-notes/27-StockAPI/src/models/user.js
+++ class-notes/27-StockAPI/src/models/user.js
@@ -3,7 +3,20 @@
     | FULLSTACK TEAM | NODEJS / EXPRESS |
 ------------------------------------------------------- */
 const { mongoose } = require('../configs/dbConnection')
+const passwordEncrypt = require("../helpers/passwordEncrypt")
 /* ------------------------------------------------------- */
+/* 
+{
+    "username": "admin",
+    "password": "aA?123456",
+    "email": "admin@site.com",
+    "firstName": "admin",
+    "lastName": "admin",
+    "isActive": true,
+    "isStaff": true,
+    "isAdmin": true
+}
+*/
 
 const UserSchema = new mongoose.Schema({
     username: {
@@ -55,5 +68,40 @@ const UserSchema = new mongoose.Schema({
     collection: "users",
     timestamps: true
 })
+/* ------------------------------------------------------- */
+//! database'e kaydetmeden hemen önce çalışan bir pre-middleware'miş
+UserSchema.pre(["save", "updateOne"], function (next) { 
+    // console.log("Pre-save run!");
+    // console.log(this);
+
+    // this; save komutunda body'deki bilgileri içeriyor. Ancak updateOne'da birçok veri döndürüyor. Biz içinden _update key'ine ulaşıyoruz.
+    // const data = this; // database'e kaydedilecek olan veri --> this // data artık bağımsiz birşeye dönüşüyor, this'deki verilerle dolmuş olan bambaşka bir veri
+
+    const data = this?._update ?? this; // updateOne'a uyarlanmış hali oluyor
+
+    // Email Control
+    // /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(data.email) // regex kodundan geçecek email // direk kontrol etmesin datadan gelen bir email varsa baksın diye aşağıdaki turnery'i yazdık
+    const isEmailValidated = data.email ? /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(data.email) : true
+
+    if (!isEmailValidated) {
+        // throw new Error("Email is not validated") // Bu şekilde ErrorHandler'a yönlendiremeyecek. O nedenle next içine yazıyoruz
+        next(new Error("Email is not validated"))
+    }
+
+    const isPasswordValidated = data.password ? /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&.]).{8,}$/.test(data.password) : true
+
+    console.log(isPasswordValidated);
+    
+
+    if (!isPasswordValidated) {
+        next(new Error("Password must be at least 8 characters long and contain at least one special character and  at least one uppercase character"))
+    }
+
+    if (data.password) {
+        this.password = passwordEncrypt(data.password)
+    }    
+    next()
+})
+
 
 module.exports = mongoose.model("User", UserSchema)
