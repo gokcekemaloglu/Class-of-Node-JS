69e7ed1bac9500178ec66a23b8c1d35c3fd299e6
gokcekemaloglu <gokce.kemaloglu@gmail.com>
2024-12-04 12:57:39 +0300

    Creater MVR for purchase, product, sales

--- class-notes/27-StockAPI/src/routes/product.js
+++ class-notes/27-StockAPI/src/routes/product.js
@@ -4,3 +4,19 @@
 ------------------------------------------------------- */
 const router = require('express').Router()
 /* ------------------------------------------------------- */
+
+const product = require('../controllers/product')
+
+// routes
+// URL /categories
+
+router.route("/")
+    .get(product.list)
+    .post(product.create)
+router.route("/:id")
+    .get(product.read)
+    .put(product.update)
+    .patch(product.update)
+    .delete(product.delete)
+
+module.exports = router
