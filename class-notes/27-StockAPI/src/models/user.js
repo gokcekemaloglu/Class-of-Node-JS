a96cc53f6681242d70e28b0b343459a1f2b3a924
gokcekemaloglu <gokce.kemaloglu@gmail.com>
2024-12-04 11:24:33 +0300

    Removed .env-sample file and added UserSchema to user.js model

--- class-notes/27-StockAPI/src/models/user.js
+++ class-notes/27-StockAPI/src/models/user.js
@@ -4,3 +4,56 @@
 ------------------------------------------------------- */
 const { mongoose } = require('../configs/dbConnection')
 /* ------------------------------------------------------- */
+
+const UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true, 
+        index: true
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        index: true
+    },
+    firstName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    lastName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    isActive: {
+        type: Boolean,
+        default: true
+    },
+    isStaff: {
+        type: Boolean,
+        default: false
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false
+    },
+    // role: {
+    //     type: String,
+    //     enum: ["client"," staff", "admin", ...]
+    // }
+}, {
+    collection: "users",
+    timestamps: true
+})
+
+module.exports = mongoose.model("User", UserSchema)
