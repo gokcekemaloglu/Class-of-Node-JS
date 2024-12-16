762239cfb7f8520475aceda5d1621e12b8283d9c
gokcekemaloglu <gokce.kemaloglu@gmail.com>
2024-12-16 11:36:55 +0300

    Refactor blog post and category routes and templates to use consistent URL structures and populate blog post data

--- class-notes/33-Template_BlogApp-2/src/controllers/view/blogPostController.js
+++ class-notes/33-Template_BlogApp-2/src/controllers/view/blogPostController.js
@@ -38,14 +38,9 @@ module.exports = {
   read: async (req, res) => {
     // req.params.postId
     // const data = await BlogPost.findById(req.params.postId)
-    const data = await BlogPost.findOne({ _id: req.params.postId }).populate(
-      "blogCategoryId",
-    ); // get Primary Data
+    const data = await BlogPost.findOne({ _id: req.params.postId }).populate("blogCategoryId"); // get Primary Data
 
-    res.status(200).send({
-      error: false,
-      result: data,
-    });
+    res.render("postRead", {post: data})
   },
 
   update: async (req, res) => {
